2b267a87a2b2335d460f99bc1b9fc1751678c480
Elena J <sjoo8503@gmail.com>
2022-09-01 14:16:21 +1200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+  </nav>
+  <RouterView />
+</template>
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+
+
+<style scoped>
+</style>
